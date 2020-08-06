38cbe263193aa74df9cd672c106deeb2e240447c
tarajaime <69281341+tarajaime@users.noreply.github.com>
2020-08-05 18:13:42 -0700

    Create prompts.js

---
+++ prompts.js
@@ -0,0 +1,5 @@
+myPrompts = [ 
+"What would make what I am doing right now easiest? ",
+"What is something that doesn’t make sense?",
+"Make a list of everything you’d like to say yes to.",
+"Write the words you need to hear." ];
